24bf0f382c11bc94f0b1957e4cb4d4393a4221b1
GameCraft910 <borah_m2003@yahoo.com>
2016-03-13 16:48:09 -0700

    Committed something you should figure out yourself

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import random
 import time
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x, y, screen):
         self.x = x
         self.y = y
+        self.screen = screen
     def __str__(self):
         return "Point at coordinates " + str(x) + ", " + str(y)
-    
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+    def display(self):
+        self.shape = pygame.draw.point(self.x, self.y)
+        self.screen.blit(self.shape)
