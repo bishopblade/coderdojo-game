3d383e7260db86f1e12e6fe0a0e92418859d7882
Admin Robinson <Admin Robinson>
2016-03-13 17:25:47 -0700

    Added Difference

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import pygame
 import random
 import time
+pygame.init()
 
-print "Welcome to Master"
+print "Welcome to Dot's Adventure"
 print "Use the arrow keys to move"
 screen = pygame.display.set_mode([640, 260])
 
-class Point(pygame.sprite.Sprite):
+coins = 50
+coins_won = random.randint(0,10)
+coins_lost = random.randint(0,3)
+
+class Player(pygame.sprite.Sprite):
     def __init__(self, x, y, screen):
         Sprite.__init__(self)
         self.x = x
         self.y = y
         self.screen = screen
+        self.color = pygame.color.Color
     def __str__(self):
         return "Point at coordinates " + str(x) + ", " + str(y)
     def move(self, x, y):
@@ -20,3 +26,6 @@ class Point(pygame.sprite.Sprite):
     def display(self):
         self.shape = pygame.draw.point(self.x, self.y)
         self.screen.blit(self.shape)
+        self.screen.update()
+    def difference (self, other):
+        return (self.x-other.x, self.y-other.y)
