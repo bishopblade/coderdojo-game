4c9df36444a3214bb9306e0173cdd7047e73d005
Admin Robinson <Admin Robinson>
2016-03-13 17:12:15 -0700

    Reupload

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import pygame
 import random
 import time
 
-class Point:
-    def __init__(self, x, y):
+print "Welcome to Master"
+print "Use the arrow keys to move"
+screen = pygame.display.set_mode([640, 260])
+
+class Point(pygame.sprite.Sprite):
+    def __init__(self, x, y, screen):
+        Sprite.__init__(self)
         self.x = x
         self.y = y
+        self.screen = screen
     def __str__(self):
         return "Point at coordinates " + str(x) + ", " + str(y)
-    
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+    def display(self):
+        self.shape = pygame.draw.point(self.x, self.y)
+        self.screen.blit(self.shape)
